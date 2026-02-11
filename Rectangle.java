ea1a7bd8fc753e96fa6241590fb61e289b83bc2f
YoussefAtGit <yb316@exeter.ac.uk>
2026-02-11 12:13:29 +0000

    completed part 5

--- Rectangle.java
+++ Rectangle.java
@@ -1,85 +1,73 @@
 // A Rectangle class
 public class Rectangle {
-  
+
   // 4 instance attributes
-  public double width;
-  public double height;
-  public double originX = 0.0;
-  public double originY = 0.0;
+  private double width;
+  private double height;
+  private double originX = 0.0;
+  private double originY = 0.0;
 
   // 1 static attributes
   public static final int NUMBER_OF_SIDES = 4;
 
-  // main constructor
-  public Rectangle(double width, double height) {
-    this.width = w;
-    this.height = h;
-    this.originX = oX;
-    this.originY = oY;
+  // main constructor (4 args)
+  public Rectangle(double width, double height, double originX, double originY) {
+    this.width = width;
+    this.height = height;
+    this.originX = originX;
+    this.originY = originY;
   }
 
-  public Rectangle() {
-    this(1, 1, 0, 0);
-  }
-
+  // second constructor (2 args)
   public Rectangle(double width, double height) {
     this(width, height, 0, 0);
   }
 
+  // third constructor (0 args)
+  public Rectangle() {
+    this(1, 1, 0, 0);
+  }
+
   // method: move the rectangle
   public void move(double dx, double dy) {
-	   originX += dx;
-	   originY += dy;
+    originX += dx;
+    originY += dy;
   }
 
   // method: compute the area of the rectangle
   public double getArea() {
     return width * height;
   }
-  
-  // method: compute the area of the rectangle
+
+  // method: compute the perimeter of the rectangle
   public double getPerimeter() {
     return 2 * (width + height);
   }
 
-  public void scale(double scaleX, double scaleY){
-    width = width * scaleX;
-    height = height * scaleY;
+  // getters
+  public double getWidth() { return width; }
+  public double getHeight() { return height; }
+  public double getOriginX() { return originX; }
+  public double getOriginY() { return originY; }
+
+  // setters (disallow negative width/height)
+  public void setWidth(double width) {
+    if (width >= 0) this.width = width;
   }
 
-  public void scale(double factor){
-    scale(factor, factor);
+  public void setHeight(double height) {
+    if (height >= 0) this.height = height;
   }
 
-  public boolean isOverlappedWith(Rectangle r){
-    double leftA = this.originX;
-    double rightA = this.originX + this.width;
-    double bottomA = this.originY;
-    double topA = this.originY + this.height;
+  public void setOriginX(double originX) { this.originX = originX; }
+  public void setOriginY(double originY) { this.originY = originY; }
 
-    double leftB = r.originX;
-    double rightB = r.originX + r.width;
-    double bottomB = r.originY;
-    double topB = r.originY + r.height;
-
-    // If separated in x or y, no overlap
-    if (rightA < leftB) return false;
-    if (rightB < leftA) return false;
-    if (topA < bottomB) return false;
-    if (topB < bottomA) return false;
-
-    return true;
-  }
-  public static boolean areOverlapping(Rectangle r1, Rectangle r2) {
-    return r1.isOverlappedWith(r2);
+  public String toString() {
+    return "Rectangle[x=" + originX + ",y=" + originY + ",w=" + width + ",h=" + height + "]";
   }
 
-  public double calcRatio() {
-    return width / height;
-}
-
-  public boolean isSquare() {
-    double ratio = calcRatio();
-    return ratio >= 0.999 && ratio <= 1.001;
+  public String toString() {
+    return "Rectangle[x=" + originX + ",y=" + originY + ",w=" + width + ",h=" + height + "]";
   }
+
 }
